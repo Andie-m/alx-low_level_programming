ac8353c496086f0d930997d2cecba94199d57a39
Andie-m <sabinakanario@gmail.com>
2023-07-17 05:45:57 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,8 +4,8 @@
  * main - Entry point of the program
  *
  * Description: This program prints all single-digit numbers of base 10
- * starting from 0, followed by a new line. It uses the putchar function to
- * print the digits and newlines.
+ * starting from 0 without newline, then followed by a newline after printing all the digits.
+ * It uses the putchar function to print the digits.
  *
  * Return: Always 0 (Success)
  */
@@ -14,10 +14,9 @@ int main(void)
 int number; /* Variable to store the digits */
 
 for (number = 0; number <= 9; number++)
-{
 putchar(number + '0'); /* Convert number to character and print */
+
 putchar('\n'); /* Print newline */
-}
 
 return (0);
 }
