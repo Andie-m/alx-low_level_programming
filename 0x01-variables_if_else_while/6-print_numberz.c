5a6b47f414c704cd6b379b9da41c2e72b6fd1bf2
Andie-m <sabinakanario@gmail.com>
2023-07-17 05:38:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * It uses the putchar function to print the digits and newlines.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar('0'); /* Print digit 0 */
+putchar('\n'); /* Print newline */
+
+putchar('1'); /* Print digit 1 */
+putchar('\n'); /* Print newline */
+
+putchar('2'); /* Print digit 2 */
+putchar('\n'); /* Print newline */
+
+putchar('3'); /* Print digit 3 */
+putchar('\n'); /* Print newline */
+
+putchar('4'); /* Print digit 4 */
+putchar('\n'); /* Print newline */
+
+putchar('5'); /* Print digit 5 */
+putchar('\n'); /* Print newline */
+
+putchar('6'); /* Print digit 6 */
+putchar('\n'); /* Print newline */
+
+putchar('7'); /* Print digit 7 */
+putchar('\n'); /* Print newline */
+
+putchar('8'); /* Print digit 8 */
+putchar('\n'); /* Print newline */
+
+putchar('9'); /* Print digit 9 */
+putchar('\n'); /* Print newline */
+
+return (0);
+}
