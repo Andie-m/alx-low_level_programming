4b7cdb719f4f126c928abd3fbac63aa13459bc4e
Andie-m <sabinakanario@gmail.com>
2023-07-17 05:43:20 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,43 +3,22 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * It uses the putchar function to print the digits and newlines.
+ * Description: This program prints all single-digit numbers of base 10
+ * starting from 0, followed by a new line. It uses the putchar function to
+ * print the digits and newlines.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-putchar('0'); /* Print digit 0 */
-putchar('\n'); /* Print newline */
+int number; /* Variable to store the digits */
 
-putchar('1'); /* Print digit 1 */
+for (number = 0; number <= 9; number++)
+{
+putchar(number + '0'); /* Convert number to character and print */
 putchar('\n'); /* Print newline */
-
-putchar('2'); /* Print digit 2 */
-putchar('\n'); /* Print newline */
-
-putchar('3'); /* Print digit 3 */
-putchar('\n'); /* Print newline */
-
-putchar('4'); /* Print digit 4 */
-putchar('\n'); /* Print newline */
-
-putchar('5'); /* Print digit 5 */
-putchar('\n'); /* Print newline */
-
-putchar('6'); /* Print digit 6 */
-putchar('\n'); /* Print newline */
-
-putchar('7'); /* Print digit 7 */
-putchar('\n'); /* Print newline */
-
-putchar('8'); /* Print digit 8 */
-putchar('\n'); /* Print newline */
-
-putchar('9'); /* Print digit 9 */
-putchar('\n'); /* Print newline */
+}
 
 return (0);
 }
+
