7db0a84e9e219abebb878ba74309deee2d36f5cc
Andie-m <sabinakanario@gmail.com>
2023-07-18 05:31:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif /* MAIN_H */
+#include "main.h"
+
+int main(void)
+{
+	print_alphabet();
+	return 0;
+}
 #include "main.h"
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
+ * Description: This function prints the lowercase alphabet from 'a' to 'z'
+ *               followed by a new line using the _putchar function.
  */
 void print_alphabet(void)
 {
@@ -15,12 +32,5 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-#ifndef MAIN_H
-#define MAIN_H
-
-/* Function prototypes */
-int _putchar(char c);
-void print_alphabet(void);
-
-#endif /* MAIN_H */
+gcc -o output_filename main.c 1-alphabet.c
 
