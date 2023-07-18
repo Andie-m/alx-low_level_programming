23dc2a739cdf3535b187cddc99ff25ea37fcbab9
Andie-m <sabinakanario@gmail.com>
2023-07-18 08:56:51 -0700

    someting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,31 +8,7 @@
  */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	print(str);
-	/*
-	ssize_t len = sizeof(str) - 1;
-	ssize_t bytes_written = 0;
-
-	while (bytes_written < len)
-	{
-		ssize_t result = _putchar(str[bytes_written]);
-		if (result == -1)
-			return 1; // Error occurred while writing 
-		bytes_written += result;
-	}
-*/
-	return 0;
+char str[] = "_putchar\n";
+print(str);
+return (0);
 }
-
-/**
- * _putchar - Custom function to write a character to the standard output.
- *
- * @c: The character to be written.
- * Return: On success, returns the character written. On error, -1 is returned.
- */
-/*int _putchar(char c)
-{
-	return write(STDOUT_FILENO, &c, 1);
-}*/
-
