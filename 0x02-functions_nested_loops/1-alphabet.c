0fc562796fab45523dfacd004ad9b0131db0ea7d
Andie-m <sabinakanario@gmail.com>
2023-07-19 06:37:45 -0700

    someting

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter); /* Use putchar from stdio.h to print the character */
-    }
-    putchar('\n'); /* Print a newline at the end */
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter); /* Use putchar from stdio.h to print the character */
+}
+putchar('\n'); /* Print a newline at the end */
 }
 
