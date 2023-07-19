37131cbf1f610142c9406b57bb9f930098eb2044
Andie-m <sabinakanario@gmail.com>
2023-07-19 06:15:51 -0700

    someting

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,10 @@
-#include "main.h"
+#include "print_functions.h" // Include the header file that declares _putchar
 
-void print_alphabet(void)
-{
-    char letter = 'a';
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
+void print_alphabet(void) {
+    char letter;
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        _putchar(letter); // Call the _putchar function to print the character
     }
-    _putchar('\n');
+    _putchar('\n'); // Print a newline at the end
 }
 
