6e9474705c4df86ef1e69e67182562a0dd105799
Andie-m <sabinakanario@gmail.com>
2023-07-19 06:31:14 -0700

    someting

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h> // Include the header file for the putchar function
+#include <stdio.h> /* Include the header file for the putchar function */
 
 void print_alphabet(void) {
     char letter;
     for (letter = 'a'; letter <= 'z'; letter++) {
-        putchar(letter); // Use putchar from stdio.h to print the character
+        putchar(letter); /* Use putchar from stdio.h to print the character */
     }
-    putchar('\n'); // Print a newline at the end
+    putchar('\n'); /* Print a newline at the end */
 }
 
