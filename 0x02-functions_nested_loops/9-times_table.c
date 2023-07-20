ecaed4268ee254b2dbda1b82a9d9ab0b1af45b87
Andie-m <sabinakanario@gmail.com>
2023-07-20 00:43:57 -0700

    someting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,33 +5,33 @@
  */
 void times_table(void)
 {
-    int i, j, product;
+int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
-            if (j == 0)
-                _putchar('0' + product);
-            else if (product < 10)
-            {
-                _putchar(' ');
-                _putchar('0' + product);
-            }
-            else
-            {
-                _putchar('0' + product / 10);
-                _putchar('0' + product % 10);
-            }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+product = i * j;
+if (j == 0)
+_putchar('0' + product);
+else if (product < 10)
+{
+_putchar(' ');
+_putchar('0' + product);
+}
+else
+{
+_putchar('0' + product / 10);
+_putchar('0' + product % 10);
+}
 
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
 
