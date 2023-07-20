dde0b70dae190584be1bf3a6fc012e7440157f49
Andie-m <sabinakanario@gmail.com>
2023-07-20 02:28:28 -0700

    someting

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Computes the sum of all the multiples of 3 or 5 below 1024 (excluded)
+ * Description: Computes the sum of all the multiples of
+ * 3 or 5 below 1024 (excluded)
  *              and prints the result.
  *
  * Return: Always 0 (Success)
