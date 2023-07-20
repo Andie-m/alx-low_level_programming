0b250e662b4b73a3fe085c7f4d8a259d5fb2496e
Andie-m <sabinakanario@gmail.com>
2023-07-20 02:44:22 -0700

    someting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,52 +6,50 @@
  */
 void print_times_table(int n)
 {
-    int i, j, result;
+	int i, j, result;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0' + result);
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (j == 0)
+				_putchar('0' + result);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (result < 100)
-                    _putchar(' ');
+				if (result < 100)
+					_putchar(' ');
 
-                if (result < 10)
-                    _putchar(' ');
+				if (result < 10)
+					_putchar(' ');
 
-                if (result >= 100)
-                {
-                    _putchar('0' + result / 100);
-                    _putchar('0' + (result / 10) % 10);
-                }
-                else if (result < 100 && result >= 10)
-                {
-                    _putchar(' ');
-                    _putchar('0' + result / 10);
-                }
-                else
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
+				if (result >= 100)
+				{
+					_putchar('0' + result / 100);
+					_putchar('0' + (result / 10) % 10);
+				}
+				else if (result < 100 && result >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + result / 10);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 
-                _putchar('0' + result % 10);
-            }
-        }
-        _putchar('\n');
-    }
+				_putchar('0' + result % 10);
+			}
+		}
+		_putchar('\n');
+	}
 }
 
