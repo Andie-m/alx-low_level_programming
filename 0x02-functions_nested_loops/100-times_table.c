d4cae35baa52aac26644d963f626934e8d4bbe6d
Andie-m <sabinakanario@gmail.com>
2023-07-20 01:08:32 -0700

    someting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,28 +1,24 @@
+/* 100-times_table.c */
+
 #include <stdio.h>
 
 void print_times_table(int n) {
 if (n < 0 || n > 15) {
-return; // Return if n is out of the valid range
+return; /* Return if n is out of the valid range */
 }
 
-for (int i = 0; i <= n; i++) {
-for (int j = 0; j <= n; j++) {
-int result = i * j;
+int i, j, result;
+for (i = 0; i <= n; i++) {
+for (j = 0; j <= n; j++) {
+result = i * j;
 
 if (j == 0) {
-printf("%2d", result); // Left-aligned for single digits
+printf("%2d", result); /* Left-aligned for single digits */
 } else {
-printf(", %2d", result); // Left-aligned for single digits
+printf(", %2d", result); /* Left-aligned for single digits */
 }
 }
 printf("\n");
 }
-}
-
-int main() {
-int n = 12; // Replace this with any value between 0 and 15
-print_times_table(n);
-
-return 0;
 }
 
