7d8f6d81544cb13e4c0ed52c7206fd41f68c9c00
Andie-m <sabinakanario@gmail.com>
2023-07-20 03:27:31 -0700

    someting

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -19,17 +19,9 @@ int second = 2;   /* Second term of the Fibonacci sequence */
 int next;         /* Variable to store the next term in the sequence */
 long sum = 2;     /* Initialize sum with the initial even term 2 */
 int count = 2;
-/**
-*Number of Fibonacci numbers printed so far
-*(starting with 1 and 2)
-*/
-
 /* Print the first two Fibonacci numbers (1 and 2) */
 printf("%d, %d, ", first, second);
 
-/** Loop to generate the remaining Fibonacci numbers and calculate
- * the sum of even terms
- */
 while (count < 98)
 {
 next = first + second;
