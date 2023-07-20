e4ecc725cebdf24d3ea21e2c91ac5a73fe076993
Andie-m <sabinakanario@gmail.com>
2023-07-20 06:59:51 -0700

    someting

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,14 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+int i;
 
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-    _putchar('\n');
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+putchar('');
 }
-
+}
+_putchar('\n');
+}
