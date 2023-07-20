d666c666f07cc75c2b1fef83bba279b9d91bb851
Andie-m <sabinakanario@gmail.com>
2023-07-20 01:31:50 -0700

    someting

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,24 +1,14 @@
-/* main_0.c */
+#include "main.h"
 
-#include <stdio.h>
+int main(void)
+{
+	print_times_table(2);
+	print_times_table(10);
+	print_times_table(13);
+	print_times_table(-2);
+	print_times_table(0);
+	print_times_table(12);
 
-/* Function prototype */
-void print_times_table(int n);
-
-int main() {
-/* Test cases */
-print_times_table(2);
-printf("\n");
-print_times_table(10);
-printf("\n");
-print_times_table(13);
-printf("\n");
-print_times_table(-2);
-printf("\n");
-print_times_table(0);
-printf("\n");
-print_times_table(12); // Changed to 12 (instead of 100) to keep the times table size manageable
-
-return (0);
+	return (0);
 }
 
