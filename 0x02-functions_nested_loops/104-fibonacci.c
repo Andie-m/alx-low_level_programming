9d0ee4cce8de5561e5a055a1d268cffd1ca2c62a
Andie-m <sabinakanario@gmail.com>
2023-07-20 05:16:07 -0700

    someting

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
 /*
- * print_fibonacci_numbers - Prints the first 'count' Fibonacci numbers.
- * @count: The number of Fibonacci numbers to print.
- *
- * This function prints the first 'count' Fibonacci numbers,
- * starting with 1 and 2,
- * separated by a comma and a space, followed by a new line.
- * It adheres to the ISO C90 standard and follows Betty coding style.
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * Return: (0)
  */
 
 int main(void)
