3b0642c6f92f6eba010a3efef2685d1c646912ab
Andie-m <sabinakanario@gmail.com>
2023-07-20 10:03:10 -0700

    someting

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,32 +9,31 @@
  */
 long largest_prime_factor(long n)
 {
-    long largest = 1;
-    long factor = 2;
+long largest = 1;
+long factor = 2;
 
-    while (n > 1)
-    {
-        if (n % factor == 0)
-        {
-            n /= factor;
-            largest = factor;
-        }
-        else
-        {
-            /* If factor is 2, increment it to 3 to check odd numbers only */
-            factor = (factor == 2) ? 3 : factor + 2;
-        }
-    }
+while (n > 1)
+{
+if (n % factor == 0)
+{
+n /= factor;
+largest = factor;
+}
+else
+{
+/* If factor is 2, increment it to 3 to check odd numbers only */
+factor = (factor == 2) ? 3 : factor + 2;
+}
+}
 
-    return largest;
+return largest;
 }
 
 int main(void)
 {
-    long number = 612852475143;
-    long largest_prime = largest_prime_factor(number);
+long number = 612852475143;
+long largest_prime = largest_prime_factor(number);
 
-    printf("%ld\n", largest_prime);
-    return 0;
-}
-	
+printf("%ld\n", largest_prime);
+return (0);
+}	
