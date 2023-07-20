90265bf150784924b0d3c3e026fae6d65a320d45
Andie-m <sabinakanario@gmail.com>
2023-07-20 01:21:46 -0700

    someting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,19 +6,17 @@
  */
 void print_times_table(int n)
 {
-	int i, j, result;
-
 	if (n < 0 || n > 15)
 		return;
 
-	for (i = 0; i <= n; i++)
+	for (int i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (int j = 0; j <= n; j++)
 		{
-			result = i * j;
+			int result = i * j;
 
 			if (j == 0)
-				_putchar('0' + result);
+				_putchar('0');
 			else
 			{
 				_putchar(',');
@@ -27,7 +25,10 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (result < 100)
 					_putchar(' ');
-				_putchar('0' + result);
+				if (result >= 100)
+					_putchar(result / 100 + '0');
+				_putchar(result / 10 % 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 		}
 		_putchar('\n');
