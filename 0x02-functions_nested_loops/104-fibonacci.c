410b79e5d0a035b2857cf76f9dc008429e6a2103
Andie-m <sabinakanario@gmail.com>
2023-07-20 03:21:53 -0700

    someting

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,41 +11,45 @@
  */
 int main(void)
 {
-    int first = 1;    /* First term of the Fibonacci sequence */
-    int second = 2;   /* Second term of the Fibonacci sequence */
-    int next;         /* Variable to store the next term in the sequence */
-    long sum = 2;     /* Initialize sum with the initial even term 2 */
-    int count = 2;    /* Number of Fibonacci numbers printed so far (starting with 1 and 2) */
+int first = 1;    /* First term of the Fibonacci sequence */
+int second = 2;   /* Second term of the Fibonacci sequence */
+int next;         /* Variable to store the next term in the sequence */
+long sum = 2;     /* Initialize sum with the initial even term 2 */
+int count = 2; /** Number of Fibonacci numbers printed so far
+*(starting with 1 and 2)
+*/
 
-    /* Print the first two Fibonacci numbers (1 and 2) */
-    printf("%d, %d, ", first, second);
+/* Print the first two Fibonacci numbers (1 and 2) */
+printf("%d, %d, ", first, second);
 
-    /* Loop to generate the remaining Fibonacci numbers and calculate the sum of even terms */
-    while (count < 98)
-    {
-        next = first + second;
-        if (next % 2 == 0)
-        {
-            sum += next;
-        }
-        first = second;
-        second = next;
-        count++;
-        /* Print the Fibonacci number with a comma and space separator */
-        if (count < 98)
-        {
-            printf("%d, ", next);
-        }
-        /* Print the last Fibonacci number without the separator */
-        else
-        {
-            printf("%d\n", next);
-        }
-    }
+/** Loop to generate the remaining Fibonacci numbers and calculate
+ * the sum of even terms
+ */
+while (count < 98)
+{
+next = first + second;
+if (next % 2 == 0)
+{
+sum += next;
+}
+first = second;
+second = next;
+count++;
+/* Print the Fibonacci number with a comma and space separator */
+if (count < 98)
+{
+printf("%d, ", next);
+}
+/* Print the last Fibonacci number without the separator */
+else
+{
+printf("%d\n", next);
+}
+}
 
-    /* Print the sum of even-valued terms in the Fibonacci sequence */
-    printf("Sum of even-valued terms: %ld\n", sum);
+/* Print the sum of even-valued terms in the Fibonacci sequence */
+printf("Sum of even-valued terms: %ld\n", sum);
 
-    return (0);
+return (0);
 }
 
