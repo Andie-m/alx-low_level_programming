1984cde7861611ba350a9e6a08fe5ebe1ed14447
Andie-m <sabinakanario@gmail.com>
2023-07-20 01:04:00 -0700

    someting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 
-void print_alphabet(void)
-{
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
+void print_times_table(int n) {
+if (n < 0 || n > 15) {
+return; // Return if n is out of the valid range
 }
-_putchar('\n');
+
+for (int i = 0; i <= n; i++) {
+for (int j = 0; j <= n; j++) {
+int result = i * j;
+
+if (j == 0) {
+printf("%2d", result); // Left-aligned for single digits
+} else {
+printf(", %2d", result); // Left-aligned for single digits
+}
+}
+printf("\n");
+}
+}
+
+int main() {
+int n = 12; // Replace this with any value between 0 and 15
+print_times_table(n);
+
+return 0;
 }
 
