9f152cc6bd52968f1e1c92378c1f60a4e2825d6d
Andie-m <sabinakanario@gmail.com>
2023-07-20 01:37:21 -0700

    someting

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+    int i;
+
+    // Test cases
+    for (i = 0; i <= 13; i++) {
+        print_times_table(i);
+        _putchar('\n');
+    }
+
+    return (0);
+}
+
