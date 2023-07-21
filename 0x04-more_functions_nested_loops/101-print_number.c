ad20229c9c574158bff82f89ea6fd6368f718704
Andie-m <sabinakanario@gmail.com>
2023-07-20 23:36:26 -0700

    something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,18 +7,7 @@
 void print_number(int n)
 {
 {
-/* Handle negative numbers */
-_putchar('-');
-_putchar('2');
-_putchar('1');
-_putchar('4');
-_putchar('7');
-_putchar('4');
-_putchar('8');
-_putchar('3');
-_putchar('6');
-_putchar('4');
-_putchar('8');
+return;
 }
 /* Handle negative numbers */
 if (n < 0)
