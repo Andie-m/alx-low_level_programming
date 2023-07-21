4e1cf18b69bdc8310cc6f3e75c177d3a9cf83251
Andie-m <sabinakanario@gmail.com>
2023-07-20 23:39:58 -0700

    something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,10 +6,6 @@
  */
 void print_number(int n)
 {
-{
--putchar('-2147483648')
-return;
-}
 /* Handle negative numbers */
 if (n < 0)
 {
@@ -20,6 +16,5 @@ n = -n;
 if (n / 10 != 0)
 print_number(n / 10);
 _putchar(n % 10 + '0');
+return;
 }
-
-
