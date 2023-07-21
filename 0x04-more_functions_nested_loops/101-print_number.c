e7bc5cb2ca7f7cee0d9e0484472f3eb0353ff650
Andie-m <sabinakanario@gmail.com>
2023-07-20 23:20:28 -0700

    something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,13 +10,11 @@ void print_number(int n)
 if (n < 0)
 {
 _putchar('-');
-return;
-{
 n = -n;
+return;
 }
 /* Recursively print each digit */
 if (n / 10 != 0)
 print_number(n / 10);
 _putchar(n % 10 + '0');
 }
-
