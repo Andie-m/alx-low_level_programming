298b522898007361aa55548d60252051d53c744e
Andie-m <sabinakanario@gmail.com>
2023-07-20 23:08:52 -0700

    something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,18 @@
 #include "main.h"
 
 /**
-
-⦁ 
-print_number - Prints an integer using _putchar.
-⦁ 
-@n: The integer to be printed.
-*/
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-/ Handle negative numbers /
+/* Handle negative numbers */
 if (n < 0)
 {
 _putchar('-');
-if (n == INT_MIN) / Special case for INT_MIN /
-{
-_putchar('2'); / Print the first digit of INT_MIN /
-n = 147483648; / Print the remaining digits of INT_MIN /
-}
-else
-{
 n = -n;
 }
-}
-/ Recursively print each digit /
+/* Recursively print each digit */
 if (n / 10 != 0)
 print_number(n / 10);
 _putchar(n % 10 + '0');
