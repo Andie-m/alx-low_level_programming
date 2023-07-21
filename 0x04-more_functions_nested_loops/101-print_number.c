4578556dee83446de7d6584f03d28f732b0e28a6
Andie-m <sabinakanario@gmail.com>
2023-07-20 22:32:55 -0700

    something

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,40 +6,18 @@
  */
 void print_number(int n)
 {
-    /* Handle special case for INT_MIN */
-    if (n == INT_MIN)
-    {
-        // Print '-' for negative, and '2', '1', '4', '7', '4', '8', '3', '6', '4', '8' individually
-        _putchar('-');
-        _putchar('2');
-        _putchar('1');
-        _putchar('4');
-        _putchar('7');
-        _putchar('4');
-        _putchar('8');
-        _putchar('3');
-        _putchar('6');
-        _putchar('4');
-        _putchar('8');
-        return;
-    }
-
-    /* Handle negative numbers */
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    /* Handle the case when n is zero */
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    /* Recursively print each digit */
-    if (n / 10 != 0)
-        print_number(n / 10);
-    _putchar(n % 10 + '0');
+if ( n  == INT_MIN)
 }
+{
+/* Handle negative numbers */
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+/* Recursively print each digit */
+if (n / 10 != 0)
+print_number(n / 10);
+_putchar(n % 10 + '0');
+}
+
