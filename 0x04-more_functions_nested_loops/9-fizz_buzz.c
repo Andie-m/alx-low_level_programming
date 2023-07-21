01d588385bfd62812739c75dd4e2d943e00951f7
Andie-m <sabinakanario@gmail.com>
2023-07-21 01:19:24 -0700

    something

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
-
 /**
- * fizzbuzz - Prints numbers from 1 to 100,
- * replacing multiples of 3 with "Fizz",
- * multiples of 5 with "Buzz", and
- * multiples of both 3 and 5 with "FizzBuzz".
- */
+
+⦁ 
+fizzbuzz - Prints numbers from 1 to 100,
+⦁ 
+replacing multiples of 3 with "Fizz",
+⦁ 
+multiples of 5 with "Buzz", and
+⦁ 
+multiples of both 3 and 5 with "FizzBuzz".
+*/
 void fizzbuzz(void)
 {
 int num;
@@ -30,6 +33,13 @@ printf("%d ", num);
 }
 }
 
+/**
+
+⦁ 
+main - Calls the fizzbuzz function and prints a newline.
+⦁ 
+Return: Always 0 (success).
+*/
 int main(void)
 {
 fizzbuzz();
