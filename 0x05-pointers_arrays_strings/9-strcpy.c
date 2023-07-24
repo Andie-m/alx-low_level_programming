5aea325d7f686cd189b9e34bcdcdb265f4196049
Andie-m <sabinakanario@gmail.com>
2023-07-24 13:34:21 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,22 @@
-/* Function to reverse a string */
-void rev_string(char *s) {
-    int length = _strlen(s);
-    int start = 0;
-    int end = length - 1;
-    while (start < end) {
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-        start++;
-        end--;
-    }
+#include "main.h"
+
+/* A function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest 
+ **/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+/* Loop through the source string and copy
+ * each character to the destination buffer */
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+/* Add the null byte at the end of the destination buffer */
+dest[i] = '\0';
+
+/* Return the pointer to the destination buffer */
+return (dest);
 }
