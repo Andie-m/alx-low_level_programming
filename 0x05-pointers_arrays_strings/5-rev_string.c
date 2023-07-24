17eecbf27b40f40b25ca30d44c0da92cc1061d72
Andie-m <sabinakanario@gmail.com>
2023-07-24 12:56:15 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,22 @@
  */
 void rev_string(char *s)
 {
-    int i, j, len;
-    char temp;
+int i, j, len;
+char temp;
 
-    /* Find the length of the string */
-    len = 0;
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+/* Find the length of the string */
+len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
 
-    /* Swap the characters from the ends to the middle */
-    for (i = 0, j = len - 1; i < j; i++, j--)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
+/* Swap the characters from the ends to the middle */
+for (i = 0, j = len - 1; i < j; i++, j--)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+}
 }
 
