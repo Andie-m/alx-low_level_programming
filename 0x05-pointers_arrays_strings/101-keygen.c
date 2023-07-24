2fa011f4df3ebdf8bcdbe291405a579443df6c1c
Andie-m <sabinakanario@gmail.com>
2023-07-24 12:40:47 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,31 +1,54 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/* Define a pool of valid characters for the password */
-const char VALID_CHARACTERS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-/**
- * generate_password - Generates a random valid password
- * @password: Pointer to the buffer to store the password
- * @length: Length of the password to be generated
- */
-void generate_password(char *password, int length)
+/* A function that generates a random valid password for the program 101-crackme */
+char *generate_password(void)
 {
-    int i;
-    int pool_size = sizeof(VALID_CHARACTERS) - 1;
+char *password;
+int i, sum, random;
 
-    /* Seed the random number generator with the current time */
-    srand((unsigned int)time(NULL));
+/* Allocate memory for the password */
+password = malloc(sizeof(char) * 100);
+if (password == NULL)
+return (NULL);
 
-    /* Generate the password */
-    for (i = 0; i < length; i++)
-    {
-        int index = rand() % pool_size;
-        password[i] = VALID_CHARACTERS[index];
-    }
+/* Initialize the random number generator */
+srand(time(NULL));
 
-    /* Null-terminate the password string */
-    password[length] = '\0';
+/* Generate characters until the sum is 2772 */
+sum = 0;
+i = 0;
+while (sum < 2772)
+{
+/* Generate a random character between ' ' and '~' */
+random = rand() % ('~' - ' ' + 1) + ' ';
+/* Add the character to the password and update the sum */
+password[i] = random;
+sum += random;
+i++;
+}
+/* Adjust the last character to make the sum exactly 2772 */
+password[i - 1] = 2772 - sum + random;
+/* Terminate the string */
+password[i] = '\0';
+
+return (password);
 }
 
+/* A main function that prints a random valid password */
+int main(void)
+{
+char *password;
+
+/* Generate and print the password */
+password = generate_password();
+if (password == NULL)
+return (1);
+printf("%s\n", password);
+
+/* Free the memory */
+free(password);
+
+return (0);
+}
