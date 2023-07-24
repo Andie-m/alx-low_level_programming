007784b15dde71308bfeae6ba3821148fc52e8e8
Andie-m <sabinakanario@gmail.com>
2023-07-24 11:15:24 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: Pointer to the string
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
+
