b173d6ac1640b924ea16d031add5a0fc223685a5
Andie-m <sabinakanario@gmail.com>
2023-07-24 14:25:44 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,61 +1,55 @@
 #include "main.h"
 
-/* A function that checks if a character is a digit */
+/**
+* is_digit - A function that checks if a character is a digit.
+* @c: The character to be checked.
+*
+* Return: 1 if the character is a digit, 0 otherwise.
+*/
+
 int is_digit(char c)
 {
-    return (c >= '0' && c <= '9');
+return (c >= '0' && c <= '9');
 }
 
-/* A function that converts a string to an integer */
+/**
+* _atoi - A function that converts a string to an integer.
+* @s: The string to be converted.
+*
+* Return: The integer representation of the string.
+*/
 int _atoi(char *s)
 {
-    int i, sign, num;
+int i, sign, num;
 
-    /* Initialize the sign and the number to 1 and 0 respectively */
-    sign = 1;
-    num = 0;
+/* Initialize the sign and the number to 1 and 0 respectively */
+sign = 1;
+num = 0;
 
-    /* Check if the string is empty */
-    if (s == NULL || *s == '\0')
-        return 0;
-
-    /* Handle the special case of "-2147483648" */
-    if (s[0] == '-' && s[1] == '2' && s[2] == '1' && s[3] == '4' &&
-        s[4] == '7' && s[5] == '4' && s[6] == '8' && s[7] == '3' &&
-        s[8] == '6' && s[9] == '4' && s[10] == '8')
-    {
-        return -2147483648;
-    }
-
-    /* Loop through the string until the end */
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        /* If the character is a minus sign, flip the sign */
-        if (s[i] == '-')
-        {
-            sign *= -1;
-        }
-        /* If the character is a digit, add it to the number */
-        if (is_digit(s[i]))
-        {
-            /* Check for integer overflow before adding the digit */
-            int digit = s[i] - '0';
-
-            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
-            {
-                return (sign == 1 ? INT_MAX : INT_MIN);
-            }
-
-            num = num * 10 + digit;
-            /* If the next character is not a digit, break the loop */
-            if (!is_digit(s[i + 1]))
-            {
-                break;
-            }
-        }
-    }
-
-    /* Return the number times the sign */
-    return (num * sign);
+/* Loop through the string until the end */
+for (i = 0; s[i] != '\0'; i++)
+{
+/* If the character is a minus sign, flip the sign */
+if (s[i] == '-')
+{
+sign *= -1;
 }
-
+/* If the character is a digit, subtract it from the number */
+if (is_digit(s[i]))
+{
+num = num * 10 - (s[i] - '0'); /* Change this line */
+/* If the next character is not a digit, break the loop */
+if (!is_digit(s[i + 1]))
+{
+break;
+}
+}
+}
+/* If the number is INT_MIN, return it as it is */
+if (num == -2147483648)
+{
+return (num);
+}
+/* Otherwise, return the number times the sign */
+return (num * sign);
+}
