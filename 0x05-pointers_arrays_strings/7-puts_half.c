cc88b0310793beb6b4d1f7cfaef74e0c5eccfe12
Andie-m <sabinakanario@gmail.com>
2023-07-24 12:22:57 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,12 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-    return length;
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return (length);
 }
 
 /**
@@ -21,24 +21,24 @@ int _strlen(char *s)
  */
 void puts_half(char *str)
 {
-    int length = _strlen(str);
-    int start;
+int length = _strlen(str);
+int start;
 
-    if (length % 2 == 0)
-    {
-        start = length / 2;
-    }
-    else
-    {
-        start = (length + 1) / 2;
-    }
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length + 1) / 2;
+}
 
-    while (str[start] != '\0')
-    {
-        _putchar(str[start]);
-        start++;
-    }
+while (str[start] != '\0')
+{
+_putchar(str[start]);
+start++;
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
 
