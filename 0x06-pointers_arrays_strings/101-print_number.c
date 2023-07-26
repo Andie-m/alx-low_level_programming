fdb1873d3bff57d6b03e9d11da5ddde10ad092af
Andie-m <sabinakanario@gmail.com>
2023-07-26 11:12:31 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,36 +1,36 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_number - Prints an integer using _putchar.
- * @n: The integer to print.
- *
- *  Return:void print_number(int n);
- */
+* print_number - prints an integer
+* @n: the integer to print
+*/
 void print_number(int n)
 {
-int num_digits, div;
+int i, j, k, d;
 
-num_digits = 1;
-div = 1;
-
+/* if n is negative, print a minus sign and make it positive */
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
 
-/* Calculate the number of digits in the number */
-while ((n / div) >= 10)
-{
-div *= 10;
-num_digits++;
-}
+/* find the number of digits in n */
+for (i = 0, j = n; j > 0; i++)
+j /= 10;
 
-/* Print each digit */
-while (div != 0)
+/* if n is zero, print zero */
+if (i == 0)
+_putchar('0');
+
+/* loop through the digits of n from left to right */
+for (j = i - 1; j >= 0; j--)
 {
-_putchar('0' + (n / div));
-n %= div;
-div /= 10;
+/* find the power of 10 for the current position */
+for (k = 1, d = 0; d < j; d++)
+k *= 10;
+
+/* print the digit at the current position */
+_putchar((n / k) % 10 + '0');
 }
 }
