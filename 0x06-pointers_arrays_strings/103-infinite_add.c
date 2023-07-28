0192eaf39870a89838c56330efa3dfbb54957ec4
Andie-m <sabinakanario@gmail.com>
2023-07-28 09:33:02 -0700

    103-infinite_add.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,81 +11,82 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i, j, k, l, m, n;
-    int carry = 0;
+int i, j, k, l, m, n;
+int carry = 0;
 
-    for (i = 0; n1[i] != '\0'; i++)
-        ;
-    for (j = 0; n2[j] != '\0'; j++)
-        ;
+for (i = 0; n1[i] != '\0'; i++);
 
-    if (i > size_r || j > size_r)
-        return (0);
+for (j = 0; n2[j] != '\0'; j++);
 
-    i--;
-    j--;
-    m = 0;
+if (i > size_r || j > size_r)
+return (0);
 
-    for (k = i, l = j; k >= 0 && l >= 0; k--, l--, m++)
-    {
-        n = (n1[k] - '0') + (n2[l] - '0') + carry;
-        if (n > 9)
-        {
-            r[m] = (n % 10) + '0';
-            carry = 1;
-        }
-        else
-        {
-            r[m] = n + '0';
-            carry = 0;
-        }
-    }
-    while (k >= 0)
-    {
-        n = (n1[k] - '0') + carry;
-        if (n > 9)
-        {
-            r[m] = (n % 10) + '0';
-            carry = 1;
-        }
-        else
-        {
-            r[m] = n + '0';
-            carry = 0;
-        }
-        m++;
-        k--;
-    }
-    while (l >= 0)
-    {
-        n = (n2[l] - '0') + carry;
-        if (n > 9)
-        {
-            r[m] = (n % 10) + '0';
-            carry = 1;
-        }
-        else
-        {
-            r[m] = n + '0';
-            carry = 0;
-        }
-        m++;
-        l--;
-    }
-    if (carry == 1)
-    {
-        r[m] = 1 + '0';
-        m++;
-    }
-    if (m + 1 >= size_r)
-        return (0);
-    r[m] = '\0';
+i--;
+j--;
+m = 0;
 
-    for (k = 0, l = m - 1; k < l; k++, l--)
-    {
-        n = r[k];
-        r[k] = r[l];
-        r[l] = n;
-    }
-    return (r);
+for (k = i, l = j; k >= 0 && l >= 0;
+k--, l--, m++)
+{
+n = (n1[k] - '0') + (n2[l] - '0') +
+carry;
+if (n > 9)
+{
+r[m] = (n % 10) + '0';
+carry = 1;
+}
+else
+{
+r[m] = n + '0';
+carry = 0;
+}
+}
+while (k >= 0)
+{
+n = (n1[k] - '0') + carry;
+if (n > 9)
+{
+r[m] = (n % 10) + '0';
+carry = 1;
+}
+else
+{
+r[m] = n + '0';
+carry = 0;
+}
+m++;
+k--;
+}
+while (l >= 0)
+{
+n = (n2[l] - '0') + carry;
+if (n > 9)
+{
+r[m] = (n % 10) + '0';
+carry = 1;
+}
+else
+{
+r[m] = n + '0';
+carry = 0;
+}
+m++;
+l--;
+}
+if (carry == 1)
+{
+r[m] = 1 + '0';
+m++;
+}
+if (m + 1 >= size_r)
+return (0);
+r[m] = '\0';
+
+for (k = 0, l = m - 1; k < l; k++, l--)
+{
+n = r[k];
+r[k] = r[l];
+r[l] = n;
+}
+return (r);
 }
