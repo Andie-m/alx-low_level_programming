77e862ef8026df27f2fdc6dc3f98cf6d5b740115
Andie-m <sabinakanario@gmail.com>
2023-07-28 09:39:59 -0700

    103-infinite_add.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,62 +1,66 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers.
- *
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
- *
- * Return: A pointer to the result, or 0 if the
- *         result cannot be stored in r.
- */
-char infinite_add(char n1, char n2, char r, int size_r)
+* infinite_add - adds two numbers
+* @n1: the first number
+* @n2: the second number
+* @r: the buffer to store the result
+* @size_r: the buffer size
+* Return: a pointer to the result or 0 if it can not be stored
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int carry = 0;
-int len1 = 0, len2 = 0, max_len = 0;
-int i, j, k;
+int i, j, k, l, m, n;
 
-while (n1[len1])
-len1++;
-while (n2[len2])
-len2++;
+for (i = 0; n1[i] != '\0'; i++);
+for (j = 0; n2[j] != '\0'; j++);
 
-max_len = (len1 > len2) ? len1 : len2;
-
-if (size_r <= max_len + 1)
+if (i > size_r || j > size_r || i + 2 >
+size_r || j + 2 > size_r)
 return (0);
 
-i = len1 - 1;
-j = len2 - 1;
-k = max_len;
+k = l = m = 0;
 
-while (i >= 0 || j >= 0 || carry)
+for (i--, j--, n = 0; n < size_r - 1;
+i--, j--, n++)
 {
-int sum = carry;
-if (i >= 0)
-sum += n1[i] - '0';
-if (j >= 0)
-sum += n2[j] - '0';
+k = (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j]
+- '0' : 0) + l;
 
-r[k] = (sum % 10) + '0';
-carry = sum / 10;
-
-i--;
-j--;
-k--;
-}
-
-if (k != -1)
+if (k > 9)
 {
-for (i = max_len; i >= 0; i--)
-r[i + 1] = r[i];
-r[0] = carry + '0';
-r[max_len + 1] = '\0';
+l = 1;
+k %= 10;
 }
 else
-{
-r[max_len + 1] = '\0';
+l = 0;
+r[n] = k + '0';
 }
+
+if (l == 1)
+{
+if (n + 1 >= size_r)
+return (0);
+r[n++] = l + '0';
+}
+
+r[n] = '\0';
+
+/* Remove leading zeros */
+for (i = 0; r[i] == '0'; i++);
+for (j = 0; r[i]; i++, j++)
+r[j] = r[i];
+r[j] = '\0';
+
+/* reverse r */
+for (i = 0, j = n - 1; i < j; i++, j--)
+{
+k = r[i];
+r[i] = r[j];
+r[j] = k;
+}
+
 return (r);
 }
+
