0ba08d0fc6dfa06e5cbcd9acdc2dda3f44ba602b
Andie-m <sabinakanario@gmail.com>
2023-07-28 08:16:44 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,37 +1,23 @@
 #include "main.h"
 
 /**
-* print_number - prints an integer
-* @n: the integer to print
-*/
+ * print_number - Print numbers as characters.
+ *
+ * @n: Integer to be printed as characters.
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
-int i, j, k, d;
+unsigned int n1;
+n1 = (n < 0) ? -n : n;
 
-/* if n is negative, print a minus sign and make it positive */
 if (n < 0)
 {
 _putchar('-');
-n = -n;
 }
 
-/* find the number of digits in n */
-for (i = 0, j = n; j > 0; i++)
-j /= 10;
-
-/* if n is zero, print zero */
-if (i == 0)
-_putchar('0');
-
-/* loop through the digits of n from left to right */
-for (j = i - 1; j >= 0; j--)
+if (n1 / 10 != 0)
 {
-/* find the power of 10 for the current position */
-for (k = 1, d = 0; d < j; d++)
-k *= 10;
-
-/* print the digit at the current position */
-_putchar((n / k) % 10 + '0');
+print_number(n1 / 10);
 }
-}
-
