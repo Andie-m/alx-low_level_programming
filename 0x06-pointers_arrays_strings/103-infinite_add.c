825ed6c9677819e889653c8399184a9a0647a32d
Andie-m <sabinakanario@gmail.com>
2023-07-29 05:49:33 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,7 +7,7 @@
 * @size_r: the buffer size
 * Return: a pointer to the result or 0 if it can not be stored
 */
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+char *infinite_add(const char *n1, const char *n2, char *r, int size_r) {
 int i, j, k, l, m, n;
 for (i = 0; n1[i] != '\0'; i++);
 for (j = 0; n2[j] != '\0'; j++);
@@ -20,13 +20,14 @@ if (k > 9) {
 l = 1;
 k %= 10;
 } else {
-l = 0;}
-r[n] = k + '0';}
+l = 0;
+}
+r[n] = k + '0';
+}
 if (l == 1) {
 if (n + 1 >= size_r)
 return (NULL);
-r[n++] = l + '0';
-}
+r[n++] = l + '0';}
 r[n] = '\0';
 for (i = 0; r[i] == '0'; i++);
 for (j = 0; r[i]; i++, j++)
@@ -36,5 +37,4 @@ for (i = 0, j = n - 1; i < j; i++, j--) {
 k = r[i];
 r[i] = r[j];
 r[j] = k;}
-return (r);
-}
+return (r);}
