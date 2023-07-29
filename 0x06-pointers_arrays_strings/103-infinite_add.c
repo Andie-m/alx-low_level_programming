a262bf927bdcdd46062062f34da860e248191f91
Andie-m <sabinakanario@gmail.com>
2023-07-29 05:17:04 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,34 +8,31 @@
 * Return: a pointer to the result or 0 if it can not be stored
 */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r){
-int i, j, k, l, m, n;
-for (i = 0; n1[i] != '\0'; i++);
-for (j = 0; n2[j] != '\0'; j++);
-if (i > size_r || j > size_r || i + 2 >
-size_r || j + 2 > size_r)
-return (0);
-k = l = m = 0;
-for (i--, j--, n = 0; n < size_r - 1;
-i--, j--, n++){
-k = (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j]
-- '0' : 0) + l;
-if (k > 9){l = 1;
-k %= 10;}
-else
-l = 0;
-r[n] = k + '0';}
-if (l == 1){
-if (n + 1 >= size_r)
-return (0);
-r[n++] = l + '0';}
-r[n] = '\0';
-for (i = 0; r[i] == '0'; i++);
-for (j = 0; r[i]; i++, j++)
-r[j] = r[i];
-r[j] = '\0';
-for (i = 0, j = n - 1; i < j; i++, j--){
-k = r[i];
-r[i] = r[j];
-r[j] = k;}
-return (r);}
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int i = strlen(n1), j = strlen(n2);
+    int k = size_r - 1, carry = 0;
+
+    r[k] = '\0';
+    i--; j--; k--;
+
+    while (i >= 0 || j >= 0) {
+        int num = carry;
+        if (i >= 0) num += n1[i--] - '0';
+        if (j >= 0) num += n2[j--] - '0';
+        if (num > 9) {
+            carry = 1;
+            num -= 10;
+        } else {
+            carry = 0;
+        }
+        if (k < 0 && (num > 0 || carry > 0)) return (0);
+        r[k--] = num + '0';
+    }
+    if (carry > 0) {
+        if (k < 0) return (0);
+        r[k--] = carry + '0';
+    }
+    return (r + k + 1);
+}
+
