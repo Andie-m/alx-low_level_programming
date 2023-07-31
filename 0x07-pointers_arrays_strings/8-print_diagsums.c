930382e929855e9c837f537376746158e89962bd
Andie-m <sabinakanario@gmail.com>
2023-07-31 11:02:55 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,28 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers.
- * @a: Pointer to the square matrix.
- * @size: The size of the square matrix.
- *
- * Format: "%d, %d\n"
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix
+ * @a: Pointer to the square matrix
+ * @size: Size of the matrix (number of rows/columns)
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int sum1 = 0;
-int sum2 = 0;
+    int i;
+    int sum1 = 0, sum2 = 0;
 
-for (i = 0; i < size; i++)
-{
-sum1 += a[i * size + i];
-sum2 += a[i * size + (size - i - 1)];
+    for (i = 0; i < size; i++)
+    {
+        sum1 += a[i * size + i];
+        sum2 += a[i * size + (size - 1 - i)];
+    }
+
+    _putchar('0' + (sum1 / 10)); /* Print tens digit of sum1 */
+    _putchar('0' + (sum1 % 10)); /* Print ones digit of sum1 */
+    _putchar(',');
+    _putchar(' ');
+    _putchar('0' + (sum2 / 10)); /* Print tens digit of sum2 */
+    _putchar('0' + (sum2 % 10)); /* Print ones digit of sum2 */
+    _putchar('\n');
 }
 
-_putchar(sum1 / '0');
-_putchar(' ');
-_putchar(sum2 / '0');
-_putchar('\n');
-}
