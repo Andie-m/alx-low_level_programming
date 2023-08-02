dc8fcaf088d78340b29446e66f22391c2866406d
Andie-m <sabinakanario@gmail.com>
2023-08-02 09:17:17 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,40 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to check.
- * Return: 1 if s is a palindrome, 0 otherwise.
- */
+* is_palindrome - checks if a string is a palindrome
+* @s: the string to check
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
-    return is_palindrome_helper(s, len);
+int len = 0;
+
+/* find the length of the string */
+while (s[len] != '\0')
+len++;
+
+/* call the helper function with the string and its length */
+return (is_palindrome_helper(s, len));
 }
 
 /**
- * is_palindrome_helper - Recursive helper function for palindrome check.
- * @s: The string to check.
- * @len: The length of the string.
- * Return: 1 if s is a palindrome, 0 otherwise.
- */
+* is_palindrome_helper - checks if a substring is a palindrome
+* @s: the substring to check
+* @len: the length of the substring
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
 int is_palindrome_helper(char *s, int len)
 {
-    if (len <= 1) /* Base case: String with length 1 or 0 is a palindrome */
-        return 1;
-    if (*s != s[len - 1]) /* Check if the first and last characters are the same */
-        return 0;
+/* base case: empty string or single character */
+if (len <= 1)
+return (1);
 
-    return is_palindrome_helper(s + 1, len - 2); /* Check the substring between the first and last characters */
+/* compare the first and last characters */
+if (s[0] != s[len - 1])
+return (0); /* not a palindrome */
+
+/* recursive case: check the substring without the first and last characters */
+return (is_palindrome_helper(s + 1, len - 2));
 }
+
 
