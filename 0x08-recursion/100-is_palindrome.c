ee436a167dfd4db6a7689b5aa4dc444dc3df6d1a
Andie-m <sabinakanario@gmail.com>
2023-08-02 09:53:52 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,25 @@
 #include "main.h"
-
-int is_palindrome_helper(char *s, int len);
+#include <stdio.h>
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: Input string.
- * Return: Length of the string.
- */
-int _strlen_recursion(char *s)
+* wildcmp - compares two strings and returns 1 if they are identical
+* @s1: the first string
+* @s2: the second string, which can contain the special character *
+* Return: 1 if s1 and s2 are identical, 0 otherwise
+*/
+int wildcmp(char *s1, char *s2)
 {
-    if (*s == '\0')
-        return 0;
-    else
-        return 1 + _strlen_recursion(s + 1);
-}
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
 
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (*(s2 + 1) == *s1 || *s1 == '\0')
+return (wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2));
+}
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+return (0);
+}
