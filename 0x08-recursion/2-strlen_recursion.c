0ed1f41f2b4b39106826850511945a739e96a0ab
Andie-m <sabinakanario@gmail.com>
2023-08-02 07:31:42 -0700

    C - Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,6 +8,6 @@ int _strlen_recursion(char *s)
 if (*s == '\0') /* Base case: If we've reached the end of the string */
 return (0);
 else
-return (1 + _strlen_recursion(s + 1)); /* Recursive call with the next character */
+return (1 + _strlen_recursion(s + 1));
 }
 
