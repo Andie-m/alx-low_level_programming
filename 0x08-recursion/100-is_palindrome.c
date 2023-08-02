7eab35a4bc136516d341fbef829d03e601bbe42c
Andie-m <sabinakanario@gmail.com>
2023-08-02 09:06:17 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,36 +1,27 @@
 #include "main.h"
 
-int check_palindrome(char *s, int len);
-
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: Input string.
- * Return: 1 if palindrome, 0 otherwise.
- */
+* is_palindrome - checks if a string is a palindrome
+* @s: the string to check
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
 int is_palindrome(char *s)
 {
-    int len;
+int len = 0, i = 0;
 
-    if (*s == '\0') /* Base case: Empty string is a palindrome */
-        return 1;
+/* find the length of the string */
+while (s[len] != '\0')
+len++;
 
-    len = _strlen_recursion(s);
-    return check_palindrome(s, len);
-}
-
-/**
- * check_palindrome - Recursive helper function for palindrome check.
- * @s: Input string.
- * @len: Length of the string.
- * Return: 1 if palindrome, 0 otherwise.
- */
-int check_palindrome(char *s, int len)
+/* compare the characters from both ends */
+while (i < len / 2)
 {
-    if (len <= 1) /* Base case: String with length 1 or 0 is a palindrome */
-        return 1;
-    if (*s != s[len - 1]) /* Check if the first and last characters are the same */
-        return 0;
-
-    return check_palindrome(s + 1, len - 2); /* Check the substring between the first and last characters */
+if (s[i] != s[len - i - 1])
+return (0); /* not a palindrome */
+i++;
 }
+
+return (1); /* is a palindrome */
+}
+
 
