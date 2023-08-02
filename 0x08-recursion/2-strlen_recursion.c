631cc403ec646ee2a6fb26053209d883136e0ab5
Andie-m <sabinakanario@gmail.com>
2023-08-02 07:28:57 -0700

    C - Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: Input string.
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0') /* Base case: If we've reached the end of the string */
+		return 0;
+	else
+		return 1 + _strlen_recursion(s + 1); /* Recursive call with the next character */
+}
+
