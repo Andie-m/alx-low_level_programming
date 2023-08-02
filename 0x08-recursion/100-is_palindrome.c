fa3646b2fe954034fc36d2f09299171e5f86199c
Andie-m <sabinakanario@gmail.com>
2023-08-02 09:08:20 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,21 +7,34 @@
 */
 int is_palindrome(char *s)
 {
-int len = 0, i = 0;
+int len = 0;
 
 /* find the length of the string */
 while (s[len] != '\0')
 len++;
 
-/* compare the characters from both ends */
-while (i < len / 2)
-{
-if (s[i] != s[len - i - 1])
-return (0); /* not a palindrome */
-i++;
+/* call the helper function with the string and its length */
+return (is_palindrome_helper(s, len));
 }
 
-return (1); /* is a palindrome */
+/**
+* is_palindrome_helper - checks if a substring is a palindrome
+* @s: the substring to check
+* @len: the length of the substring
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
+int is_palindrome_helper(char *s, int len)
+{
+/* base case: empty string or single character */
+if (len <= 1)
+return (1);
+
+/* compare the first and last characters */
+if (s[0] != s[len - 1])
+return (0); /* not a palindrome */
+
+/* recursive case: check the substring without the first and last characters */
+return (is_palindrome_helper(s + 1, len - 2));
 }
 
 
