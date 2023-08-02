a93878e99a3b357dd7bc48447fa0c918288857e7
Andie-m <sabinakanario@gmail.com>
2023-08-02 07:55:38 -0700

    C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0') /* Base case: If we've reached the end of the string */
-return;
-else
+if (*s != '\0')
 {
-_print_rev_recursion(s + 1); /* Recursive call with the next character */
-_putchar(*s); /* Print the current character after the recursive call */
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
- 
+
