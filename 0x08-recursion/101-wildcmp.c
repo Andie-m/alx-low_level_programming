107ef1867da9743d92fa7439fcf25fc1bfc421de
Andie-m <sabinakanario@gmail.com>
2023-08-02 09:43:43 -0700

    C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,31 +9,20 @@
 */
 int wildcmp(char *s1, char *s2)
 {
-/* base case: both strings are empty */
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 
-/* if s2 contains *, check the next characters */
 if (*s2 == '*')
 {
-/* if the next character of s2 is also *, skip it */
 if (*(s2 + 1) == '*')
 return (wildcmp(s1, s2 + 1));
-
-/* if the next character of s2 matches s1, or s1 is empty, check the rest of the strings */
+if (wildcmp(s1, s2 + 1))
+return (1);
 if (*(s2 + 1) == *s1 || *s1 == '\0')
 return (wildcmp(s1, s2 + 1));
-
-/* otherwise, move to the next character of s1 */
 return (wildcmp(s1 + 1, s2));
 }
-
-/* if the current characters of s1 and s2 match, check the rest of the strings */
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
-
-/* otherwise, the strings are not identical */
 return (0);
 }
-
-
