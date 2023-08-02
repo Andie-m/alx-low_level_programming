0abb7df21c2f0272ed0082f7d66ac10da37d1286
Andie-m <sabinakanario@gmail.com>
2023-08-02 07:52:13 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0') /* Check if the current character is not the null terminator */
-	{
-		_putchar(*s); /* Print the current character */
-		_puts_recursion(s + 1); /* Call the function recursively with the next character */
-	}
-	else
-	{
-		_putchar('\n'); /* If we have reached the end of the string, print a new line */
-	}
+    if (*s != '\0')
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }
 }
 
